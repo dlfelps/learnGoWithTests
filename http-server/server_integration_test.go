2db8e3b633a269b2b87c32fef67475cff0515031
dlfelps <dlfelps@gmail.com>
2025-07-21 16:45:53 +0000

    v5 final http-server

---
+++ http-server/server_integration_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+  store := NewInMemoryPlayerStore()
+  server := PlayerServer{store}
+  player := "Pepper"
+
+  pepperWin := newPostWinRequest(player)
+  server.ServeHTTP(httptest.NewRecorder(), pepperWin)
+  server.ServeHTTP(httptest.NewRecorder(), pepperWin)
+  server.ServeHTTP(httptest.NewRecorder(), pepperWin)
+
+  response := httptest.NewRecorder()
+  server.ServeHTTP(response, newGetScoreRequest(player))
+  assertStatus(t, response.Code, http.StatusOK)
+
+  assertResponseBody(t, response.Body.String(), "3")
+}
